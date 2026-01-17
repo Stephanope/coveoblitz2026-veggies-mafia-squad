cd2d3a108a86b213d94ebee94fa7774dc96f4efc
Stephanope <stefansucatu@gmail.com>
2026-01-17 11:14:11 -0500

    ajout spawner

--- java/src/main/java/codes/blitz/game/bot/Bot.java
+++ java/src/main/java/codes/blitz/game/bot/Bot.java
@@ -1,38 +1,58 @@
 package codes.blitz.game.bot;
 
-import codes.blitz.game.generated.*;
+import codes.blitz.game.generated;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Bot {
+  boolean initialized = false;
+  Position[][] nutrientPositions;
   Random random = new Random();
 
   public Bot() {
     System.out.println("Initializing your super mega duper bot");
   }
 
-  /*
-   * Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
-   */
-  public List<Action> getActions(TeamGameState gameMessage) {
-    List<Action> actions = new ArrayList<>();
+  /* Here is where the magic happens, for now the moves are not very good. I bet you can do better 😉*/
+  public List<Action> getActions(TeamGameState gameMessage) 
+  {
+    if (!initialized) {
+      nutrientPositions = getNutrientPositions(gameMessage.world().map());
+      initialized = true;
+    // }
 
-    TeamInfo myTeam = gameMessage.world().teamInfos().get(gameMessage.yourTeamId());
-    if (myTeam.spawners().isEmpty()) {
-      actions.add(new SporeCreateSpawnerAction(myTeam.spores().getFirst().id()));
-    } else if (myTeam.spores().isEmpty()) {
-      actions.add(new SpawnerProduceSporeAction(myTeam.spawners().getFirst().id(), 20));
-    } else {
-      actions.add(
-          new SporeMoveToAction(
-              myTeam.spores().getFirst().id(),
-              new Position(
-                  random.nextInt(gameMessage.world().map().width()),
-                  random.nextInt(gameMessage.world().map().height()))));
+    List<Action> actions = new ArrayList<>();
+    myTeamId = gameMessage.yourTeamId();
+    Spores mySpores = gameMessage.world().teams().get(myTeamId).spores();
+    Spawners mySpawners = gameMessage.world().teams().get(myTeamId).spawners();
+
+    for (int i = 0; i < mySpores.size(); i++) {
+      Spores spore = mySpores.get(i);
+      if (spore.biomass() == spawnerCost(mySpawners.size()) || (spore.biomass() == 1 && mySpawners.size() == 0)) {
+        actions.add(new SporeCreateSpawnerAction(spore.id()));
+      }      
     }
 
     // You can clearly do better than the random actions above. Have fun!!
     return actions;
   }
+
+  public Position[] getNutrientPositions(GameMap map) {
+    Position[] positions = null;
+    for (int i= 0; i < map.width(); i++){
+      for (int j= 0; j < map.height(); j++){
+        positions.add(new Position(i,j));
+        System.out.println("Nutrient found at " + i + "," + j);
+      }
+    }
+
+    return positions;
+  }
+
+  public int spawnerCost(int spawnersOnMap) {
+    return (int) Math.pow(2, spawnersOnMap) - 1;
+  }
+
+
 }
